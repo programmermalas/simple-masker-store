d627f3eb215ee6cba4c1b78d4aae70f2aea6ede2
Irfan Hadian <irfan.hadian10@gmail.com>
2020-04-06 13:54:22 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -24,136 +24,23 @@ Route::get('/', function () {
     return view( 'pages.guest.welcome', compact('products') );
 });
 
-Route::get('/product/{product}', function ( $product ) {
-    $product    = App\Models\Product::where( 'slug', $product )->first();
+Route::get('/product/{product}', 'Guest\ProductController@detail');
 
-    return view( 'pages.guest.product', compact('product') );
-});
+Route::post('/product/{product}', 'Guest\ProductController@store');
 
-Route::post('/product/{product}', function ( Request $request, $product ) {
-    $product    = App\Models\Product::where( 'slug', $product )->first();
+Route::get('/cart', 'Guest\CartController@index');
 
-    \Cart::add(array(
-        'id'            => $product->id,
-        'name'          => $product->title,
-        'price'         => $product->price,
-        'quantity'      => $request->quantity,
-        'attributes'    => [
-            'weight'    => $product->weight
-        ]
-    ));
+Route::post('/cart', 'Guest\CartController@update');
 
-    return redirect()->back()->with('success', 'Item added to cart');
-});
+Route::get('/cart/{id}/delete', 'Guest\CartController@destroy');
 
-Route::get('/cart', function () {
-    $carts  = \Cart::getContent();
+Route::get('/checkout', 'Guest\CheckoutController@index');
 
-    return view( 'pages.guest.cart', compact('carts', 'weight') );
-});
+Route::get('/order', 'Guest\OrderController@index');
 
-Route::get('/cart/{id}/delete', function ( $id ) {
-    $carts  = \Cart::remove( $id );
+Route::post('/order', 'Guest\OrderController@store');
 
-    return redirect()->back()->with('warning', 'Item removed in cart');
-});
-
-Route::post('/cart', function ( Request $request ) {
-    foreach ( $request->ids as $key => $id ) {
-        Cart::update($id, [
-            'quantity' => [
-                'relative'  => false,
-                'value'     => $request->quantities[$key]
-            ]
-        ]);
-    }
-
-    return redirect()->back()->with('info', 'Cart updated!');
-});
-
-Route::get('/checkout', function () {
-    $carts  = \Cart::getContent();
-    $weight = 0;
-
-    foreach ( $carts as $cart ) {
-        $weight += $cart->attributes['weight'] * $cart->quantity;
-    }
-
-    return view( 'pages.guest.checkout', compact('weight') );
-});
-
-Route::post('/order', function ( Request $request ) {
-    $request->validate([
-        'first_name'    => 'required|max:25',
-        'last_name'     => 'required|max:25',
-        'province'      => 'required',
-        'city'          => 'required',
-        'street'        => 'required|max:100',
-        'postcode'      => 'required|max:10',
-        'phone'         => 'required|max:15',
-        'email'         => 'required|email',
-        'shipping'      => 'required',
-        'weight'        => 'required',
-        'total'         => 'required'
-    ]);
-
-    try {
-        $count  = App\Models\Order::withTrashed()->count();
-        $invoice    = Carbon\Carbon::now()->format('d/m/Y/') . str_pad($count + 1, 4, '0', STR_PAD_LEFT);
-    
-        $order = App\Models\Order::create([
-            'id'            => Str::uuid(),
-            'invoice'       => $invoice,
-            'first_name'    => $request->first_name,
-            'last_name'     => $request->last_name,
-            'province_id'   => $request->province,
-            'city_id'       => $request->city,
-            'street'        => $request->street,
-            'postcode'      => $request->postcode,
-            'phone'         => $request->phone,
-            'email'         => $request->email,
-        ]);
-    
-        App\Models\Bill::create([
-            'id'        => Str::uuid(),
-            'order_id'  => $order->id,
-            'shipping'  => $request->shipping,
-            'weight'    => $request->weight,
-            'total'     => $request->total
-        ]);
-    
-        $items  = \Cart::getContent();
-    
-        foreach ( $items as $item ) {
-            App\Models\OrderProduct::create([
-                'id'        => Str::uuid(),
-                'order_id'  => $order->id,
-                'product_id'=> $item->id,
-                'quantity'  => $item->quantity
-            ]);
-        }
-
-        $items  = \Cart::clear();
-
-        Mail::to( $order->email )->send( new OrderMail( $order ) );
-    } catch (\Exception $e) {
-        return redirect()->back()->with('error', $e->getMessage());
-    }
-
-    return view( 'pages.guest.order.index', compact('order') );
-});
-
-Route::get('/order/detail', function ( Request $request ) {
-    if ( $request->invoice ) {
-        $order  = App\Models\Order::where( 'invoice', $request->invoice )->first();
-    
-        if (!$order) {
-            return redirect( '/order/detail' )->with('info', 'Order not found!');
-        }
-    }
-
-    return view( 'pages.guest.order.detail', compact('order') );
-});
+Route::get('/order/detail', 'Guest\OrderController@detail');
 
 Route::get('/payment', 'Guest\PaymentController@index');
 
