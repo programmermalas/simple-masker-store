649d9ce73da0d760e9f773b8b7b337d6d00e9ea1
Irfan Hadian <irfan.hadian10@gmail.com>
2020-04-06 16:03:30 +0700

    remove paid option

--- resources/views/pages/admin/order/edit.blade.php
+++ resources/views/pages/admin/order/edit.blade.php
@@ -33,7 +33,6 @@
         
                             <select name="status" class="custom-select @if ($errors->has('status')) is-invalid @endif" id="status">
                                 <option value="waited" {{ $order->status == 'waited' ? 'selected' : null }}>Waited</option>
-                                <option value="paid" {{ $order->status == 'paid' ? 'selected' : null }}>Paid</option>
                                 <option value="delivered" {{ $order->status == 'delivered' ? 'selected' : null }}>Delivered</option>
                                 <option value="canceled" {{ $order->status == 'canceled' ? 'selected' : null }}>Canceled</option>
                             </select>
