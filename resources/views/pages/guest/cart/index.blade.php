6dcf5796b13cfdb8b2036ec6e4027c2038b48abc
Irfan Hadian <irfan.hadian10@gmail.com>
2020-04-06 13:54:47 +0700

    changes pages hirarky

---
+++ resources/views/pages/guest/cart/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.guest')
+
+@section('content')
+<div class="container">
+    <div class="pt-3">
+        @include('partials._alerts')
+    </div>
+
+    <div class="row pt-0 pt-md-3">
+        <div class="col-sm-12 col-md-8 pt-3 pt-md-0">
+            <form action="{{ url('/cart') }}" method="post" id="cart">
+                @csrf
+                
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Product</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @php
+                            $no = 0;
+                        @endphp
+                        @foreach ($carts as $cart)
+                        <tr>
+                            <th scope="row">{{ ++$no }}</th>
+                            <td>{{ $cart->name }}</td>
+                            <td>Rp {{ number_format( $cart->price, 0, '.', ',' ) }}</td>
+                            <td>
+                                <input name="ids[]" type="hidden"value={{ $cart->id }}>
+                                <input name="quantities[]" type="number" name="form-control" id="quantity" min="10" value="{{ $cart->quantity }}" step="10">
+                            </td>
+                            <td>
+                                <a href="{{ url( "/cart/{$cart->id}/delete" ) }}" class="btn btn-sm btn-danger rounded-circle">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-secondary" form="cart">Update cart</button>
+                </div>
+            </form>
+        </div>
+
+        <div class="col-sm-12 col-md-4 pt-3 pt-md-0 d-flex flex-column justify-content-between">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Totals</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td>Total Quantity</td>
+                        <td>{{ \Cart::getTotalQuantity() }}</td>
+                    </tr>
+
+                    <tr>
+                        <td>Sub Total</td>
+                        <td>Rp {{ number_format( \Cart::getSubTotal(), 0, ',', '.' ) }}</td>
+                    </tr>
+
+                    <tr>
+                        <td>Total</td>
+                        <td>Rp {{ number_format( \Cart::getTotal(), 0, ',', '.' ) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <a href="{{ url('/checkout') }}" class="btn btn-secondary" >Proceed to checkout</a>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/pages/guest/product/detail.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.guest')
+
+@section('content')
+<div class="container">
+    <div class="pt-3">
+        @include('partials._alerts')
+    </div>
+
+    <div class="row pt-0 pt-md-3">
+        <div class="col-sm-12 col-md-6 pt-3 pt-md-0">
+            <div class="d-flex justify-content-center align-items-center shadow-sm" style="height: 480px; overflow: hidden;">
+                <img src="/storage/images/500/{{ $product->productImage->name }}" class="d-block w-100" style="background-size: cover; background-repeat: no-repeat;" alt="{{ $product->productImage->name }}">
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-6 pt-3 pt-md-0">
+            <form action="{{ url("/product/{$product->slug}") }}" method="post">
+                @csrf
+
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item bg-light"><h5>{{ $product->title }}</h5></li>
+
+                    <li class="list-group-item bg-light">
+                        <p>{{ $product->description }}</p>    
+                    </li>
+
+                    <li class="list-group-item bg-light">Stock: {{ $product->stock }}</li>
+                    
+                    <li class="list-group-item bg-light">Weight: {{ $product->weight }} Gram</li>
+
+                    <li class="list-group-item bg-light">Rp {{ number_format( $product->price, 0, '.', ',' ) }}</li>
+                    
+                    <li class="list-group-item bg-light">
+                        <div class="form-group mb-0 row">
+                            <label for="quantity" class="col-sm-2 col-form-label">Quantity</label>
+
+                            <div class="col-sm-10">
+                                <input name="quantity" type="number" class="form-control @if ($errors->has('title')) is-invalid @endif" id="quantity" min="100" step="100" value="{{ old('title') ?? '100' }}">
+
+                                @if ($errors->has('title'))
+                                    <div class="invalid-feedback">
+                                        {{$errors->first('title')}}
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item bg-light">
+                        <button type="submit" class="btn btn-secondary d-block w-100">Add to cart</button>
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
