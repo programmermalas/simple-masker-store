c4eec56fb8b0685beb07223e15eab17d0c1137ff
Irfan Hadian <irfan.hadian10@gmail.com>
2020-04-13 07:59:01 +0700

    update order print

--- resources/views/pdfs/order.blade.php
+++ resources/views/pdfs/order.blade.php
@@ -26,29 +26,52 @@
             <thead>
                 <tr>
                     <th>No</th>
+                    <th style="text-alignt: center">Invoice</th>
                     <th style="text-align: left;">Buyer</th>
                     <th style="text-align: left;">Province</th>
                     <th style="text-align: left;">City</th>
                     <th style="text-align: left;">Street</th>
-                    <th>Quantity</th>
                     <th>Status</th>
+                    <th>Marketing</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
                 </tr>
             </thead>
             <tbody>
                 @php
                     $no = 0;
+                    $total = 0;
+                    $quantity = 0;
                 @endphp
                 @foreach ($datas as $data)
                     <tr>
                         <td style="text-align: center;">{{ ++$no }}</td>
+                        <td style="text-align: center;">{{ $data->invoice }}</td>
                         <td>{{ $data->first_name }} {{ $data->last_name }}</td>
                         <td>{{ $data->province->name }}</td>
                         <td>{{ $data->city->name }}</td>
                         <td>{{ $data->street }}</td>
-                        <td style="text-align: center;">{{ $data->orderProducts->sum('quantity') }}</td>
                         <td style="text-align: center;">{{ $data->status }}</td>
+                        <td style="text-align: center;">{{ $data->user->name ?? null }}</td>
+                        <td style="text-align: center;">
+                            @php
+                                $quantity += $data->orderProducts->sum('quantity');
+                            @endphp
+                            {{ $data->orderProducts->sum('quantity') }}
+                        </td>
+                        <td style="text-align: right;">
+                            @php
+                                $total += $data->bill->total;
+                            @endphp
+                            {{ number_format( $data->bill->total, 0, '.', ',' ) }}
+                        </td>
                     </tr>
                 @endforeach
+                <tr>
+                    <td colspan="8" style="text-align: center;">Total</td>
+                    <td style="text-align: center;">{{ $quantity }}</td>
+                    <td style="text-align: center;">{{ number_format( $total, 0, '.', ',' ) }}</td>
+                </tr>
             </tbody>
         </table>
     </body>
