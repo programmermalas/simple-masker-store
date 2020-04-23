c77ad30724ca31e6ef0e56c2040ff733ea3290fb
Irfan Hadian <irfan.hadian10@gmail.com>
2020-04-23 10:43:15 +0700

    update welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $products   = App\Models\Product::get();
+    $products   = App\Models\Product::orderBy('created_at', 'desc')->get();
 
     return view('pages.guest.welcome', compact('products'));
 });
